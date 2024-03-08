f0a05f2a30eb7fa77eff1bf41daca9fec26fdb40
Dominic <andro5@web.de>
2024-03-08 08:27:53 +0100

    create conflict!

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello Cgn-java-24-1");
         System.out.println("Hello Everybody");
+        System.out.println("Hello there!");
     }
 }
